d77797fb29428c3a55a6837cdbd9666fafb971c6
alphin <alphinpoly8@gmail.com>
2023-05-29 12:15:31 +0530

    game created using onclick

--- java.js
+++ java.js
@@ -1,54 +1,63 @@
 let choice = ["rock", "paper", "scissors"];
-let computerChoice = getComputerChoice();
 let playerChoice;
-let choose;
-let r,p,s;
+let computerChoice;
 let cc = 0;
 let pc = 0;
+let choose;
 
+
+function compare(computerChoice, playerChoice){
+    if(computerChoice == "rock" && playerChoice == "scissors"){
+        cc += 1;
+        document.getElementById('result').innerHTML= "Computer wins"
+    }else if(computerChoice == "scissors" && playerChoice == "paper"){
+        cc += 1;
+        document.getElementById('result').innerHTML= "Computer wins"
+    }else if(computerChoice == "paper" && playerChoice == "rock"){
+        cc += 1;
+        document.getElementById('result').innerHTML= "Computer wins"
+    }else if(computerChoice == "scissors" && playerChoice == "rock"){
+        pc += 1;
+        document.getElementById('result').innerHTML= "You wins"
+    }else if(computerChoice == "rock" && playerChoice == "paper"){
+        pc += 1;
+        document.getElementById('result').innerHTML= "You wins"
+    }else if(computerChoice == "paper" && playerChoice == "scissors"){
+        pc += 1;
+        document.getElementById('result').innerHTML= "You wins"
+    }else if(computerChoice == playerChoice){
+        document.getElementById('result').innerHTML = "Draw"
+    }
+}
+
+function getPlayerChoice(choose){
+    if(choose == "rock"){
+        playerChoice = "rock"
+    }else if(choose == 'paper'){
+        playerChoice = 'paper'
+    }else if(choose == 'scissors'){
+        playerChoice = 'scissors'
+    }
+    console.log(playerChoice)
+};
 
 function getComputerChoice(){
     let i = Math.floor(Math.random()* 3)
     return choice[i];
 }
-
-function compare(computerChoice, playerChoice){
-    if(computerChoice == "rock" && playerChoice == "scissors"){
-        cc += 1;
-        console.log("Computer wins");
-    }else if(computerChoice == "scissors" && playerChoice == "paper"){
-        cc += 1;
-        console.log("Computer wins");
-    }else if(computerChoice == "paper" && playerChoice == "rock"){
-        cc += 1;
-        console.log("Computer wins");
-    }else if(computerChoice == "scissors" && playerChoice == "rock"){
-        pc += 1;
-        console.log("You wins");
-    }else if(computerChoice == "rock" && playerChoice == "paper"){
-        pc += 1;
-        console.log("You wins");
-    }else if(computerChoice == "paper" && playerChoice == "scissors"){
-        pc += 1;
-        console.log("You wins");
+let j = 0;
+function play(){
+    if(j<5){
+        computerChoice = getComputerChoice();
+        console.log(computerChoice)
+        compare(computerChoice, playerChoice);
     }
+    else if(j==5 && pc > cc){
+        document.getElementById('result').innerHTML = "Player wins by "+pc
+    }else if(j==5 && cc > pc){
+        document.getElementById('result').innerHTML = "Computer wins by "+cc
+    }else if(j > 5){
+        document.getElementById('result').innerHTML = "Press F5 to rePlay "
+    }
+    j++;
 }
-
-document.getElementById("btr").addEventListener('click',rock);
-document.getElementById("btp").addEventListener('click',paper);
-document.getElementById("bts").addEventListener('click',scissors);
-
-function rock(){
-    playerChoice = "rock";
-}
-function paper(){
-    playerChoice = "paper";
-}
-function scissors(){
-    playerChoice = "scissors";
-}
-
-
-getComputerChoice();
-compare(computerChoice,playerChoice);
-console.log(playerChoice)
